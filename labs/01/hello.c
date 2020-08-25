d3bb035d90837633dfdd53c9342846de6c8c9ad9
CaroMedina <A00825117@itesm.mx>
2020-08-24 23:34:06 -0500

    <A00825117>-homework-01

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
+    printf("My name is Carolina Medina Medrano\n");
     return 0; 
 }
